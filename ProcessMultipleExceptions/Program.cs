250b318a4490e2ee1956eccdc6fc7b6a121026ca
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-30 16:01:36 -0400

    Add multiple catches to main() method try block in appropriate order.

--- ProcessMultipleExceptions/Program.cs
+++ ProcessMultipleExceptions/Program.cs
@@ -10,6 +10,27 @@ namespace ProcessMultipleExceptions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Handling Multiple Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+
+            try
+            {
+                // Trip Arg out of range exception.
+                myCar.Accelerate(-10);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
